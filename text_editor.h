c08dfcfd1ca9b54340b642b20fc8622ae66ed313
vilhola2 <villekytola@proton.me>
2024-12-26 17:45:24 +0200

    clean up text_editor.h

--- text_editor.h
+++ text_editor.h
@@ -4,16 +4,13 @@
 #ifndef TEXT_EDITOR_H
 #define TEXT_EDITOR_H
 
-#define ERR 0
-
 void clear_input_buffer(void);
 
 char *file_read(char *filename);
 void file_write(char *filename, char *input);
 
-int32_t str_get_line_count(char *buffer);
 char *str_input(FILE *stream);
+int32_t str_get_line_count(char *input);
 void str_print(char *input, int32_t *line_count);
-void str_reduce(char *input);
 
 #endif
